b663bd3230dc6cbdeea43470eddf2673176da122
BBeelze <timothyalli18@gmail.com>
2025-10-19 04:39:00 +0800

    edit/update button done for category

--- happ/views.py
+++ happ/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from happ.models import Category
 from happ.forms import CategoryForm
 from django.urls import reverse_lazy
@@ -17,6 +17,12 @@ class CategoryList(ListView):
     paginate_by = 5
 
 class CategoryCreateView(CreateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'category_form.html'
+    success_url = reverse_lazy('category-list')
+
+class CategoryUpdateView(UpdateView):
     model = Category
     form_class = CategoryForm
     template_name = 'category_form.html'
--- projectsite/urls.py
+++ projectsite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from happ.views import HomePageView, CategoryList, CategoryCreateView
+from happ.views import HomePageView, CategoryList, CategoryCreateView, CategoryUpdateView
 from happ import views
 
 urlpatterns = [
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('category_list', CategoryList.as_view(), name='category-list'),
     path('category_list/add', CategoryCreateView.as_view(), name='category-add'),
+    path('category_list/<pk>',CategoryUpdateView.as_view(), name='category-update'),
 ]
 
